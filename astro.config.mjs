09c75cd4d3459278f4a11f2d6815b03b405d97a1
Rohit <rr1796@dseu.ac.in>
2024-10-11 15:10:53 +0530

    ready to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,13 +11,7 @@ export default defineConfig({
 				github: 'https://github.com/withastro/starlight',
 			},
 			sidebar: [
-				{
-					label: 'Installation',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
+				
 				
 				{
 					label: 'Components',
